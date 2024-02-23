ad22e65b733489900f0fb9aa5fed6d540025bc3d
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-23 22:17:24 +0000

    simplify tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/**/*.tsx`,
+    `./src/**/*.{ts,tsx}`,
   ],
   theme: {
     extend: {},
