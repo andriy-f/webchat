329318adbf844c2f1eca7581492edfa68fe27dcd
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-23 18:44:35 +0000

    update metadata

--- client/gatsby-config.ts
+++ client/gatsby-config.ts
@@ -2,8 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `K-Chat client`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `K-Chat`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
