ed420d91588860dcd7533c6269989425427741cb
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-03-09 22:34:51 +0000

    main icon

--- client/src/features/layout/RegularMainMenu.tsx
+++ client/src/features/layout/RegularMainMenu.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavLink from '../navigation/NavLink'
 import { mainMenuItems } from '../navigation/mainMenuItems'
+import { ChatBubbleBottomCenterTextIcon } from '@heroicons/react/24/outline'
 
 const RegularMainMenu = () => {
   return (
     <div className='flex items-center'>
       <div className='flex-shrink-0'>
-        <div className='text-white text-lg'>K-Chat</div>
+        <ChatBubbleBottomCenterTextIcon className='h-8 w-8 text-white' />
       </div>
       <div>
         <div className='ml-10 flex items-baseline space-x-4'>
