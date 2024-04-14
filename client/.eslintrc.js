23c42c099ca1db47d797803c3f1110038a4648d3
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-04-14 17:06:11 +0000

    code style

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-    globals: {
-        __PATH_PREFIX__: true,
-    },
-    ignorePatterns: ['node_modules/', '.cache/', 'public/'],
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint',
-        'react'
-    ],
-    'rules': {
-        semi: ['error', 'never'],
-        quotes: ['error', 'single'],
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-    },
-    'settings': {
-        'react': {
-            'version': 'detect',
-        }
-    },
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  ignorePatterns: ['node_modules/', '.cache/', 'public/'],
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react'
+  ],
+  'rules': {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    }
+  },
 }
